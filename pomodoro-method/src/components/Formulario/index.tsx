5f453acd0cbce2c91915e3f20dfab5a55b8923c9
Luis <luis.moreno.dol@gmail.com>
2022-10-27 20:49:14 -0300

    Implementação de um botão

---
+++ pomodoro-method/src/components/Formulario/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Formulario extends React.Component {
+    render() {
+        return (
+            <form>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </form>
+        )
+    }
+}
