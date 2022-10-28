9bf234ef87826a4a0dc87c236dd13703d130a3d6
Luis <luis.moreno.dol@gmail.com>
2022-10-28 15:54:52 -0300

    Criando o formulário

--- pomodoro-method/src/components/Formulario/index.tsx
+++ pomodoro-method/src/components/Formulario/index.tsx
@@ -1,16 +1,40 @@
 import React from "react";
+import Botao from "../Botao";
 
 class Formulario extends React.Component {
     render() {
         return (
             <form>
                 <div>
-
+                    <label htmlFor="tarefa">
+                        Adicione o que você deseja fazer
+                    </label>
+                    <input
+                        type="text"
+                        name="tarefa"
+                        id="tarefa"
+                        placeholder="O que você quer fazer?"
+                        required
+                    />
                 </div>
                 <div>
-                    
+                    <label htmlFor="tempo">
+                        Tempo
+                    </label>
+                    <input
+                        type="time"
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
+                    />
                 </div>
+                <Botao />
             </form>
         )
     }
 }
+
+export default Formulario;
