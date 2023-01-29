57697de7f8636af43f7dd272322f9553c76c9827
Luis Roberto Moreno Dollazi <luis.moreno.dol@gmail.com>
2023-01-29 20:05:30 -0300

    Finalizando

--- pomodoro-method/src/components/Lista/Item/index.tsx
+++ pomodoro-method/src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefa'
-import style from '../Lista.module.scss'
+import style from './Item.module.scss'
 
 interface Props extends ITarefa {
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -14,7 +14,7 @@ export default function Item(
         selecionaTarefa
     }: Props) {
     return (
-        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} onClick={() => selecionaTarefa({
+        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`} onClick={() => !completado && selecionaTarefa({
             tarefa,
             tempo,
             selecionado,
@@ -27,6 +27,7 @@ export default function Item(
             <span>
                 {tempo}
             </span>
+            {completado && <span className={style.concluido} aria-label='tarefa completada'></span>}
         </li>
     )
 }
--- pomodoro-method/src/pages/App.tsx
+++ pomodoro-method/src/pages/App.tsx
@@ -10,20 +10,40 @@ function App() {
   const [selecionado, setSelecionado] = useState<ITarefa>()
 
   function selecionaTarefa(tarefaSelecionada: ITarefa) {
-    setSelecionado(tarefaSelecionada)   
+    setSelecionado(tarefaSelecionada)
     setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
       ...tarefa,
       selecionado: tarefa.id === tarefaSelecionada.id ? true : false
-    }))) 
+    })))
+  }
+
+  function finalizarTarefa() {
+    if (selecionado) {
+      setSelecionado(undefined)
+      setTarefas(tarefasAnteriores =>
+        tarefasAnteriores.map(tarefa => {
+          if (tarefa.id === selecionado.id) {
+            return {
+              ...tarefa,
+              selecionado: false,
+              completado: true
+            }
+          }
+          return tarefa
+        }))
+    }
   }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista 
+      <Lista
         tarefas={tarefas}
-        selecionaTarefa={selecionaTarefa} 
+        selecionaTarefa={selecionaTarefa}
       />
-      <Cronometro />
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
