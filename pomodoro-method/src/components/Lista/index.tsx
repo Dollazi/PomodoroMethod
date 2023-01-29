4a4e1b2bd463265dd6f9919f269e016928c7edc2
Luis Roberto Moreno Dollazi <luis.moreno.dol@gmail.com>
2023-01-29 20:09:14 -0300

    Refatorando

--- pomodoro-method/src/components/Lista/index.tsx
+++ pomodoro-method/src/components/Lista/index.tsx
@@ -1,31 +1,29 @@
-import { ITarefa } from "../../types/tarefa";
-import Item from "./Item";
+import React from 'react';
+import { ITarefa } from '../../types/tarefa';
+import Item from './Item';
 import style from './Lista.module.scss';
 
 interface Props {
-    tarefas: ITarefa[],
-    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
 }
 
-function Lista({ tarefas, selecionaTarefa } : Props) {    
 
-    return (
-        <aside className={style.listaTarefas}>
-            <h2>Atividades de controle</h2>
-            <ul>
-                {tarefas.map((item) => (
-                    <Item
-                        selecionaTarefa={selecionaTarefa}
-                        key={item.id}                        
-                        tarefa={item.tarefa}
-                        tempo={item.tempo} 
-                        selecionado={false} 
-                        completado={false} id={""} 
-                    />
-                ))}
-            </ul>
-        </aside>
-    )
+function Lista({ tarefas, selecionaTarefa }: Props) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map(item => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
+            {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
 }
 
 export default Lista;
